c6c4cd40ee34372abb6af11e7a3ff1f8316f0976
Marcelo Lima <marcelo@telenordigital.com>
2019-03-16 10:10:10 +0100

    Remove compression

--- src/server.cpp
+++ src/server.cpp
@@ -38,95 +38,6 @@ namespace {
     return file;
   }
 
-//  /* Compress from file source to file dest until EOF on source.
-//   def() returns Z_OK on success, Z_MEM_ERROR if memory could not be
-//   allocated for processing, Z_STREAM_ERROR if an invalid compression
-//   level is supplied, Z_VERSION_ERROR if the version of zlib.h and the
-//   version of the library linked do not match, or Z_ERRNO if there is
-//   an error reading or writing the files. */
-//  int def(FILE *source, FILE *dest, int level)
-//  {
-//    int ret, flush;
-//    unsigned have;
-//    z_stream strm;
-//    unsigned char in[CHUNK];
-//    unsigned char out[CHUNK];
-//
-//    /* allocate deflate state */
-//    strm.zalloc = Z_NULL;
-//    strm.zfree = Z_NULL;
-//    strm.opaque = Z_NULL;
-//    ret = deflateInit(&strm, level);
-//    if (ret != Z_OK)
-//      return ret;
-//
-//    /* compress until end of file */
-//    do {
-//      strm.avail_in = fread(in, 1, CHUNK, source);
-//      if (ferror(source)) {
-//        (void)deflateEnd(&strm);
-//        return Z_ERRNO;
-//      }
-//      flush = feof(source) ? Z_FINISH : Z_NO_FLUSH;
-//      strm.next_in = in;
-//
-//      /* run deflate() on input until output buffer not full, finish
-//         compression if all of source has been read in */
-//      do {
-//        strm.avail_out = CHUNK;
-//        strm.next_out = out;
-//        ret = deflate(&strm, flush);    /* no bad return value */
-//        assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
-//        have = CHUNK - strm.avail_out;
-//        if (fwrite(out, 1, have, dest) != have || ferror(dest)) {
-//          (void)deflateEnd(&strm);
-//          return Z_ERRNO;
-//        }
-//      } while (strm.avail_out == 0);
-//      assert(strm.avail_in == 0);     /* all input will be used */
-//
-//      /* done when last data in file processed */
-//    } while (flush != Z_FINISH);
-//    assert(ret == Z_STREAM_END);        /* stream will be complete */
-//
-//    /* clean up and return */
-//    (void)deflateEnd(&strm);
-//    return Z_OK;
-//  }
-//
-//  inline void compress() {
-//    std::ifstream f;
-//    f.open("");
-//    f.
-//    gzFile output = gzopen("file.gz", "wb");
-//    gzwrite(output, "my decompressed data", strlen("my decompressed data"));
-//    gzclose(output);
-//  }
-
-  inline restinio::transforms::zlib::params_t::format_t
-  compressionSupported(restinio::request_handle_t request) noexcept {
-    auto compression = request->header().get_field(restinio::http_field::accept_encoding, "");
-
-    if (compression.find("gzip") != std::string::npos) {
-      return restinio::transforms::zlib::params_t::format_t::gzip;
-    } else if (compression.find("deflate") != std::string::npos) {
-      return restinio::transforms::zlib::params_t::format_t::deflate;
-    } else {
-      return restinio::transforms::zlib::params_t::format_t::identity;
-    }
-  }
-
-  constexpr inline auto contentEncodingToken(restinio::transforms::zlib::params_t::format_t format) {
-    switch (format) {
-      case restinio::transforms::zlib::params_t::format_t::gzip:
-        return "gzip";
-      case restinio::transforms::zlib::params_t::format_t::deflate:
-        return "deflate";
-      case restinio::transforms::zlib::params_t::format_t::identity:
-        return "identity";
-    }
-  }
-
   inline restinio::request_handling_status_t forbidden(server::Request request) noexcept {
     return server::fail(request, restinio::status_forbidden());
   }
@@ -142,21 +53,6 @@ namespace {
   inline restinio::request_handling_status_t notFound(server::Request request) noexcept {
     return server::fail(request, restinio::status_not_found());
   }
-
-  inline restinio::request_handling_status_t sendToLogin(server::Request request) noexcept {
-    request->create_response(restinio::status_temporary_redirect())
-        .append_header(restinio::http_field::location, "https://auth.mflima.com/login")
-        .connection_close()
-        .done();
-    return restinio::request_accepted();
-  }
-
-  template <typename T>
-  inline void compressIfAvailable(restinio::response_builder_t<T> response,
-                                  restinio::transforms::zlib::params_t::format_t format) noexcept {
-    response.append_header(restinio::http_field::content_encoding, contentEncodingToken(format));
-  }
-
 }
 
 namespace server {
