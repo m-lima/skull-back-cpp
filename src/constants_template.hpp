60c69860111dbeae339a3e1a9ba4159d78366756
m-lima <marcelowind@gmail.com>
2019-03-17 18:01:40 +0100

    Add build script

---
+++ src/constants_template.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <array>
+
+namespace constant {
+  namespace path {
+    constexpr const auto GET_QUICK = "";
+    constexpr const auto GET_SKULL = "";
+    constexpr const auto POST_SKULL = "";
+  }
+
+  namespace file {
+    constexpr const auto ROOT = "";
+    constexpr const auto QUICK = "";
+    constexpr const auto SKULL = "";
+  }
+
+  namespace header {
+    constexpr const auto X_USER = "";
+    constexpr const auto ORIGIN = "";
+  }
+
+  namespace cors {
+    constexpr const auto ALLOW_CORS = false;
+    constexpr const auto ORIGIN = "";
+  }
+
+  namespace user {
+    constexpr const auto UNKNOWN = "";
+  }
+}
