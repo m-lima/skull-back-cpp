0f4cdaf7e69f9d570bc4996d83a5ba39f2f2d9f3
m-lima <marcelowind@gmail.com>
2021-01-02 17:10:08 +0000

    Simplify gitignore

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 docker build -t skull .
 docker stop skull
 docker rm skull
-. ./cfg/data_location.env
 docker create \
   --name skull \
-  --volume "${DATA_LOCATION}":/data \
+  --volume "${PWD}/data":/data \
   --net fly \
   skull
